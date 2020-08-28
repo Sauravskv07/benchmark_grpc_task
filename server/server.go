0f85a5aa8d6d02b21004d92d538c309729a0c250
Sauravskv07 <sauravvirmaniskv@gmail.com>
2020-08-28 18:07:11 +0530

    2. Minor Bugs

--- server/server.go
+++ server/server.go
@@ -26,6 +26,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+    "io"
 	"log"
 	"net"
 	"google.golang.org/grpc"
@@ -46,17 +47,17 @@ type taskServiceServer struct {
 
 // GetFeature returns the feature at the given point.
 func(s *taskServiceServer) GetTaskPage(ctx context.Context, in *pb.TaskRequest) (*pb.TaskResponse, error){
-
     page_size:=in.num_tasks
     comp_id:=in.comp_id
     resp:=new(pb.TaskResponse)
     resp.count=0
     taskArrayPtr := make([]*pb.Task, page_size)
     resp.tasks = taskArrayPtr
-    for i:=0; i!=0 && queues[comp_id].Empty()==false; i--{
-        task, err:=queues[comp_id].Get()
+    count:=0
+    for i:=0; i!=0 && s.queues[comp_id].Empty()==false; i--{
+        task, err:=s.queues[comp_id].Get()
         if err==nil{
-            resp.tasks[i]=&Task
+            resp.tasks[i]=&task
         }else{
             return nil,err
         } 
@@ -70,22 +71,22 @@ func (s *taskServiceServer) AddTask(stream pb.TaskService_AddTaskServer) error{
     
     for {
         task, err:=stream.Recv()
-        if err==io.EOF(){
+        if err==io.EOF{
             return stream.SendAndClose(&pb.TaskAddAck{
-                message:"Done Adding Task",
+                status:"Done Adding Task",
             })
         }
         if(err!=nil){
             return err
         }
         
-        if queues[task.comp_id].Len()==queues[task.comp_id].Cap() {
+        if s.queues[task.comp_id].Len()==s.queues[task.comp_id].Cap() {
             return stream.SendAndClose(&pb.TaskAddAck{
                 status:"Queue Overflow! No more tasks can be added at this point. Try Later!",
             })
         }
 
-        err = queues[task.comp_id].Put()
+        err = s.queues[task.comp_id].Put()
         
         if err!=nil{
             return err
@@ -108,9 +109,9 @@ func (s *taskServiceServer) GetTaskStream(stream pb.TaskService_GetTaskStreamSer
 
         if(page_size<=-1){
             
-            for queues[comp_id].Empty()==false{
+            for s.queues[comp_id].Empty()==false{
             
-                task,err:=queues[comp_id].Get()
+                task,err:=s.queues[comp_id].Get()
             
                 if err!=nil{
                     
@@ -129,9 +130,9 @@ func (s *taskServiceServer) GetTaskStream(stream pb.TaskService_GetTaskStreamSer
             return nil
         
         } else{
-            for ;(queues[comp_id].Empty()==false && page_size>0);page_size--{
+            for ;(s.queues[comp_id].Empty()==false && page_size>0);page_size--{
 
-                task,err:=queues[comp_id].Get()
+                task,err:=s.queues[comp_id].Get()
             
                 if err!=nil{
                     
